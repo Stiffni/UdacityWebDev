1a8e65f3fd00a8d439ab558ec5cd94bca3b6e686
Steff Freeman <steffaniefreeman@gmail.com>
2019-01-13 14:02:51 -0500

    List places now off screen on small viewports

--- neighbourhoodMap/src/components/mapcontainer.js
+++ neighbourhoodMap/src/components/mapcontainer.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
+  handleClick(e){
+    console.log(e);
+  }
   render() {
     const style = {
       width: '100%',
@@ -10,8 +13,10 @@ export class MapContainer extends Component {
     return (
       <div className="map-nav-wrapper">
         <div className="navigation">
-          <nav role="navigation" aria-label="Main" className="drawer">
-            <a className="menu">
+          <nav role="navigation" aria-label="Main">
+            <a
+              className="menu"
+              onClick={this.handleClick}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
               </svg>
